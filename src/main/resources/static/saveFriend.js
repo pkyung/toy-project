7843310255394af43be0a555855c745f0c88586c
EunKyung Lee <2dmsrud2002@naver.com>
2023-09-27 16:43:11 +0900

    friend request modify

--- src/main/resources/static/saveFriend.js
+++ src/main/resources/static/saveFriend.js
@@ -5,9 +5,6 @@ button.onclick = function () {
     $.ajax({
         type: "POST",
         url: `/friend/${friendId.value}`,
-        success: function () {
-            console.log("좋아요 완료");
-        }
     })
     button.disabled = true;
 }
